b1fea1dd4a92039563b3b2974b0d679bc3d499d3
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-29 17:28:44 +0100

    Creating users and login check. Also returning mesage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, request, make_response, jsonify
 from flask_cors import CORS
 from passlib.hash import sha256_crypt
 import pyodbc
@@ -23,53 +23,82 @@ connection_string = 'Driver=%s;Server=%s;Database=%s;Uid=%s;Pwd=%s;Encrypt=%s;Tr
 @app.route('/', methods=["post"])
 def hello_world():
     print('Hello World!')
-    return "hello"
+    return make_response(jsonify({'message': 'Hello'}), 200)
+
+
+@app.route('/user/login', methods=["post"])
+def login_user():
+    conn, curs = connect_conn()
+
+    if request.is_json:
+        user = request.get_json()["user"]
+
+        try:
+            curs.execute("SELECT * FROM users WHERE mail = ?", user['mail'])
+            row = curs.fetchone()
+
+            while row:
+                print("password:" + row[2])
+                if sha256_crypt.verify(user["pwd"], row[2]):
+                    return make_response(jsonify({'message': 'User Exists'}), 202)
+                row = curs.fetchone()
+
+            return make_response(jsonify({'message': 'User Doesn\'t Exists'}), 401)
+        except BaseException as e:
+            return make_response(jsonify({'message': 'Unknown Error'}), 400)
+        finally:
+            close_conn(conn)
 
 
 @app.route('/user/create', methods=["post"])
 def create_user():
-    conn = pyodbc.connect(connection_string)
-    curs = conn.cursor()
-
-    #
-    # print("Select:")
-    # curs.execute("SELECT * FROM users")
-    # row = curs.fetchone()
-    #
-    # while row:
-    #     print(row)
-    #     row = curs.fetchone()
-    #
-    # password = sha256_crypt.encrypt("password")
-    # password2 = sha256_crypt.encrypt("password")
-    #
-    # print(password)
-    # print(password2)
-    #
-    # print(sha256_crypt.verify("password", password))
+    conn, curs = connect_conn()
 
     if request.is_json:
         user = request.get_json()["user"]
-        pwd_hash = sha256_crypt.hash(user["password"])
-        # print(user["username"])
-        # print(user["password"])
-        # print(pwd_hash)
-        # print(sha256_crypt.verify(user["password"], pwd_hash))
+        pwd_hash = sha256_crypt.hash(user["pwd"])
 
         try:
             print("Insert new user:")
-            curs.execute("INSERT INTO users (name ,password, mail) VALUES (?,?,?)", user["username"], pwd_hash,
-                         user["mail"])
-            if conn.commit():
-                print("Successfully inserted: " + curs.fetchone()[0])
-        except:
-            print("Failed inserting user:")
+            if not check_if_user_exists(curs, user["mail"]):
+                curs.execute("INSERT INTO users (name, password, mail) VALUES (?, ?, ?)", user["name"], pwd_hash, user["mail"])
+                conn.commit()
+                print("Successfully inserted: " + user["name"])
+                return make_response(jsonify({'message': 'User created successfully'}), 201)
+            else:
+                print("User exists")
+                return make_response(jsonify({'message': 'User already exists'}), 400)
+        except BaseException as e:
+            print("Failed inserting user...")
+            return make_response(jsonify({'message': 'Unknown Error'}), 400)
         finally:
-            print("Closing connection...")
-            if conn.close():
-                print("Connection closed")
+            close_conn(conn)
 
-    return ""
+
+def connect_conn():
+    conn = pyodbc.connect(connection_string)
+    curs = conn.cursor()
+    return conn, curs
+
+
+def close_conn(conn):
+    print("Closing connection...")
+    conn.close()
+    print("Connection closed")
+
+
+def check_if_user_exists(curs, mail):
+    curs.execute("SELECT mail FROM users")
+    row = curs.fetchone()
+
+    while row:
+        if row[0] == mail:
+            return True
+        row = curs.fetchone()
+
+    return False
+
+
 
 
 if __name__ == '__main__':
