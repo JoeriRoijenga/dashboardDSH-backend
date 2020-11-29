263371df0e4199b75c45f75998a84350258626ef
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-29 17:40:18 +0100

    Cleaning code

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ connection_string = 'Driver=%s;Server=%s;Database=%s;Uid=%s;Pwd=%s;Encrypt=%s;Tr
 
 @app.route('/', methods=["post"])
 def hello_world():
-    print('Hello World!')
-    return make_response(jsonify({'message': 'Hello'}), 200)
+    return make_response(jsonify({'message': 'Hello World'}), 200)
 
 
 @app.route('/user/login', methods=["post"])
@@ -33,21 +32,21 @@ def login_user():
     if request.is_json:
         user = request.get_json()["user"]
 
-        try:
-            curs.execute("SELECT * FROM users WHERE mail = ?", user['mail'])
-            row = curs.fetchone()
-
-            while row:
-                print("password:" + row[2])
-                if sha256_crypt.verify(user["pwd"], row[2]):
-                    return make_response(jsonify({'message': 'User Exists'}), 202)
+        if check_if_user_exists(curs, user['mail']):
+            try:
+                curs.execute("SELECT password FROM users WHERE mail = ?", user['mail'])
                 row = curs.fetchone()
 
-            return make_response(jsonify({'message': 'User Doesn\'t Exists'}), 401)
-        except BaseException as e:
-            return make_response(jsonify({'message': 'Unknown Error'}), 400)
-        finally:
-            close_conn(conn)
+                while row:
+                    if sha256_crypt.verify(user["pwd"], row[0]):
+                        return make_response(jsonify({'message': 'User Exists'}), 202)
+                    row = curs.fetchone()
+
+                return make_response(jsonify({'message': 'User Doesn\'t Exists'}), 401)
+            except BaseException as e:
+                return make_response(jsonify({'message': 'Unknown Error'}), 400)
+            finally:
+                close_conn(conn)
 
 
 @app.route('/user/create', methods=["post"])
@@ -59,20 +58,21 @@ def create_user():
         pwd_hash = sha256_crypt.hash(user["pwd"])
 
         try:
-            print("Insert new user:")
             if not check_if_user_exists(curs, user["mail"]):
                 curs.execute("INSERT INTO users (name, password, mail) VALUES (?, ?, ?)", user["name"], pwd_hash, user["mail"])
                 conn.commit()
-                print("Successfully inserted: " + user["name"])
                 return make_response(jsonify({'message': 'User created successfully'}), 201)
             else:
-                print("User exists")
                 return make_response(jsonify({'message': 'User already exists'}), 400)
         except BaseException as e:
-            print("Failed inserting user...")
             return make_response(jsonify({'message': 'Unknown Error'}), 400)
         finally:
             close_conn(conn)
+
+
+@app.route('/user/get', methods=["GET"])
+def get_users():
+    return make_response(jsonify({'message': 'OK'}), 200)
 
 
 def connect_conn():
@@ -82,13 +82,11 @@ def connect_conn():
 
 
 def close_conn(conn):
-    print("Closing connection...")
     conn.close()
-    print("Connection closed")
 
 
 def check_if_user_exists(curs, mail):
-    curs.execute("SELECT mail FROM users")
+    curs.execute("SELECT mail FROM users WHERE mail = ?", mail)
     row = curs.fetchone()
 
     while row:
@@ -97,8 +95,6 @@ def check_if_user_exists(curs, mail):
         row = curs.fetchone()
 
     return False
-
-
 
 
 if __name__ == '__main__':
