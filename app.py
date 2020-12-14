7af078aa07643712c16a47a5f7ca5a0810082703
joeriroijenga <roijenga.joeri@gmail.com>
2020-12-14 11:02:11 +0100

    Blueprints working

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
-from flask import Flask, Blueprint, request, make_response, jsonify
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-import pyodbc
 
 from routes import users_bp
 
 app = Flask(__name__)
 CORS(app)
-
-driver = "{ODBC Driver 17 for SQL Server}"
-server = "tcp:iot-db-roijenga.database.windows.net,1433"
-database = "iot-db-weather-pi"
-username = "iot-joeri-roijenga"
-password = "cO1WfBYif7eA"
-encryption = "yes"
-certificate = "no"
-timeout = "30"
-
-connection_string = 'Driver=%s;Server=%s;Database=%s;Uid=%s;Pwd=%s;Encrypt=%s;TrustServerCertificate=%s;Connection Timeout=%s;' % (
-    driver, server, database, username, password, encryption, certificate, timeout)
-
-
-@app.route('/', methods=["post"])
-def hello_world():
-    return make_response(jsonify({'message': 'Hello World'}), 200)
-
-
-def connect_conn():
-    conn = pyodbc.connect(connection_string)
-    curs = conn.cursor()
-    return conn, curs
-
-
-def close_conn(conn):
-    conn.close()
 
 
 def register_blueprints():
--- routes/users.py
+++ routes/users.py
@@ -1,124 +1,124 @@
 from flask import Blueprint, request, make_response, jsonify
 from passlib.hash import sha256_crypt
-from app import connect_conn, close_conn
+from connection_settings import connect, close
 
 users_bp = Blueprint("users", __name__)
 
 
-@users_bp.route('/user/login', methods=["post"])
+@users_bp.route('/login', methods=["post"])
 def login_user():
-    conn, curs = connect_conn()
+    connection, cursor = connect()
 
     if request.is_json:
         user = request.get_json()["user"]
 
-        if check_if_user_exists_by_mail(curs, user['mail']):
+        if check_if_user_exists_by_mail(cursor, user['mail']):
             try:
-                curs.execute("SELECT password FROM users WHERE mail = ?;", user['mail'])
-                row = curs.fetchone()
+                cursor.execute("SELECT password FROM users WHERE mail = ?;", user['mail'])
+                row = cursor.fetchone()
 
                 while row:
                     if sha256_crypt.verify(user["pwd"], row[0]):
                         return make_response(jsonify({'message': 'User Exists'}), 202)
-                    row = curs.fetchone()
+                    row = cursor.fetchone()
 
                 return make_response(jsonify({'message': 'User Doesn\'t Exists'}), 401)
             except:
                 return make_response(jsonify({'error': 'Unknown Error'}), 400)
             finally:
-                close_conn(conn)
+                close(connection)
         return make_response(jsonify({'error': 'Wrong Credentials'}), 401)
     return make_response(jsonify({'error': 'Wrong Format'}), 400)
 
 
-@users_bp.route('/user/create', methods=["post"])
+@users_bp.route('/create', methods=["post"])
 def create_user():
     if request.is_json:
         user = request.get_json()["user"]
         pwd_hash = sha256_crypt.hash(user["pwd"])
 
         try:
-            conn, curs = connect_conn()
+            connection, cursor = connect()
 
-            if not check_if_user_exists_by_mail(curs, user["mail"]):
-                curs.execute("INSERT INTO users (name, password, mail) VALUES (?, ?, ?);", user["name"], pwd_hash,
+            if not check_if_user_exists_by_mail(cursor, user["mail"]):
+                cursor.execute("INSERT INTO users (name, password, mail) VALUES (?, ?, ?);", user["name"], pwd_hash,
                              user["mail"])
-                conn.commit()
+                connection.commit()
                 return make_response(jsonify({'message': 'User created successfully'}), 201)
             return make_response(jsonify({'message': 'User already exists'}), 400)
         except:
             return make_response(jsonify({'error': 'Unknown Error'}), 400)
         finally:
-            close_conn(conn)
+            close(connection)
 
     return make_response(jsonify({'error': 'Wrong Format'}), 400)
 
 
-@users_bp.route('/users/get/all', methods=["GET"])
+@users_bp.route('/get/all', methods=["GET"])
 def get_users():
     try:
-        conn, curs = connect_conn()
+        connection, cursor = connect()
 
-        curs.execute("SELECT name, mail FROM users;")
-        users = fetch_all_users(curs)
+        cursor.execute("SELECT name, mail FROM users;")
+        users = fetch_all_users(cursor)
         return make_response(jsonify({'users': users}), 200)
     except:
         return make_response(jsonify({'message': 'Unknown Error'}), 400)
     finally:
-        close_conn(conn)
+        close(connection)
 
 
-@users_bp.route('/users/get/<string:_id>', methods=["GET"])
+@users_bp.route('/get/<string:_id>', methods=["GET"])
 def get_user(_id):
     try:
-        conn, curs = connect_conn()
+        connection, cursor = connect()
 
-        if check_if_user_exists_by_id(curs, _id):
-            curs.execute("SELECT name, mail FROM users WHERE id = ?;", _id)
+        if check_if_user_exists_by_id(cursor, _id):
+            cursor.execute("SELECT name, mail FROM users WHERE id = ?;", _id)
 
-            return make_response(jsonify({'user': fetch_all_users(curs)}), 200)
+            return make_response(jsonify({'user': fetch_all_users(cursor)}), 200)
         return make_response(jsonify({'message': 'User Doesn\'t Exists'}), 401)
     except:
         return make_response(jsonify({'error': 'Unknown Error'}), 400)
     finally:
-        close_conn(conn)
+        close(connection)
 
 
-@users_bp.route('/users/update/<string:_id>', methods=["PUT"])
+@users_bp.route('/update/<string:_id>', methods=["PUT"])
 def update_user(_id):
     if request.is_json:
         user = request.get_json()["user"]
 
         try:
-            conn, curs = connect_conn()
-            if check_if_user_exists_by_id(curs, _id):
-                curs.execute("UPDATE users SET name = ?, mail = ?;", user["name"], user["mail"])
+            connection, cursor = connect()
+            if check_if_user_exists_by_id(cursor, _id):
+                cursor.execute("UPDATE users SET name = ?, mail = ?;", user["name"], user["mail"])
                 return make_response(jsonify({'message': 'OK'}), 200)
             return make_response(jsonify({'message': 'User Doesn\'t Exists'}), 401)
         except:
             return make_response(jsonify({'message': 'Unknown Error'}), 400)
         finally:
-            close_conn(conn)
+            close(connection)
 
     return make_response(jsonify({'message': 'Wrong Format'}), 400)
 
 
-def fetch_all_users(curs):
+def fetch_all_users(cursor):
     dictionary = {}
-    row = curs.fetchone()
+    row = cursor.fetchone()
     count = 0
 
     while row:
         dictionary[count] = {'name': row[0], 'mail': row[1]}
-        row = curs.fetchone()
+        row = cursor.fetchone()
         count += 1
 
     return dictionary
 
 
-def check_if_user_exists_by_id(curs, _id):
-    return True if curs.execute("SELECT COUNT(id) FROM users WHERE id = ?", _id).fetchone()[0] == 1 else False
+def check_if_user_exists_by_id(cursor, _id):
+    return True if cursor.execute("SELECT COUNT(id) FROM users WHERE id = ?", _id).fetchone()[0] == 1 else False
 
 
-def check_if_user_exists_by_mail(curs, mail):
-    return True if curs.execute("SELECT COUNT(mail) FROM users WHERE mail = ?", mail).fetchone()[0] == 1 else False
+def check_if_user_exists_by_mail(cursor, mail):
+    return True if cursor.execute("SELECT COUNT(mail) FROM users WHERE mail = ?", mail).fetchone()[0] == 1 else False
