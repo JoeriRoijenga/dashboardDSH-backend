a7a463539ed4c810dc72c2c53519a99732ffb0b2
joeriroijenga <roijenga.joeri@gmail.com>
2021-01-07 09:53:46 +0100

    added save function

--- routes/settings.py
+++ routes/settings.py
@@ -7,9 +7,9 @@ settings_bp = Blueprint("settings", __name__)
 @settings_bp.route('/get/general', methods=["GET"])
 @jwt_required
 def get_general_settings():
-    connection, cursor = connect()
-
     try:
+        connection, cursor = connect()
+
         cursor.execute("SELECT \"type\", \"on\" FROM general_settings;")
         
         returnData = []
@@ -26,3 +26,20 @@ def get_general_settings():
         return jsonify({'error': 'Unknown Error'}), 400
     finally:
         close(connection)
+
+@settings_bp.route('/save/general', methods=["PUT"])
+@jwt_required
+def save_general_settings():
+    if (request.is_json):
+        settings = request.get_json()["settings"]
+        print(settings)
+        try:
+            connection, cursor = connect()
+            # for item in settings:
+                # print(item["type"] + " : " + item["on"])
+            # cursor.execute("UPDATE general_settings SET \"type\" = '?', \"on\" = '?';", settings[])
+            return jsonify({'message': "success"}), 200
+        except:
+            return jsonify({'error': 'Unknown Error'}), 400
+        finally:
+            close(connection)
